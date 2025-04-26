6a5b05d3e4925d659cbb041ac2f88a3515f47047
moteki-0035-git <148021294+moteki-0035-git@users.noreply.github.com>
2025-04-26 14:46:25 +0900

    Add files via upload

---
+++ 20250426/MyFirst0426/MyFirst0426/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirst0426
+//
+//  Created by classmac2025 on 2025/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 20250426/MyFirst0426/MyFirst0426/MyFirst0426App.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirst0426App.swift
+//  MyFirst0426
+//
+//  Created by classmac2025 on 2025/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirst0426App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
