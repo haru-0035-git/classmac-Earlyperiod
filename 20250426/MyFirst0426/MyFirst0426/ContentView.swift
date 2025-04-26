e23ddc93d1cf021dbd1c96ff3aa80a0e37943c76
moteki-0035-git <148021294+moteki-0035-git@users.noreply.github.com>
2025-04-26 16:17:53 +0900

    Add files via upload

--- 20250426/MyFirst0426/MyFirst0426/ContentView.swift
+++ 20250426/MyFirst0426/MyFirst0426/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var outputText:String = "Hello World"
+    @State var outputboolen : Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text(outputText)
+                .font(.largeTitle)
+            Button("切り替えボタン"){
+                if outputboolen == true{
+                    outputText = "Hello World"
+                    outputboolen = false
+                }else{
+                    outputText = "Hi Swift!"
+                    outputboolen = true
+                }
+            }
+            .padding()
+            .background(.blue)
+            .foregroundStyle(.white)
         }
-        .padding()
     }
 }
 
